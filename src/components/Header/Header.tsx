dc874493547b804554bfdf7e68d7a2b5175429eb
Ilya Skorodko <iskorodko@ozon.ru>
2023-10-24 19:24:03 +0300

    the most important feature

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,26 @@
 import { FunctionComponent } from "react";
+import SevaImg from '../../../public/seva.jpeg';
 import styles from './styles.module.css';
 
 export const Header: FunctionComponent = () => {
+  function speak() {
+    alert('Ну что, размялись?');
+  }
+
   return (
     <header className={styles.header}>
       <div className={styles.inner}>
         <h1 className={styles.title}>
           Seva Gym App
         </h1>
+        <button className={styles.button} onClick={speak}>
+          <img 
+            className={styles.image}
+            src={SevaImg.src}
+            alt="Сева"
+            width="80px" 
+          />
+        </button>
       </div>
     </header>
   )
